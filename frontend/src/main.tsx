a4d01c0a9561c2da0cb331b74f97633cae6a5785
Roberta-Brasil <robertabrasil.dev@gmail.com>
2022-07-18 19:25:08 -0300

    Project Clean

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Basic Structure of the React application
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 )
