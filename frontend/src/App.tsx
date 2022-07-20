bba13c6e49ed6185ec95ceeec66e409c228c048b
Roberta-Brasil <robertabrasil.dev@gmail.com>
2022-07-20 07:51:11 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
- 
-  return(
-  
-  <h1>Olá Mundo!</h1>)
+import NotificationButton from "./components/NotificationButton"
 
+function App() {
+
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+            
+    </>
+  )
 }
 
 export default App
